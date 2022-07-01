eec83b9c29667ca990eb5576d55c7f87f10b7dc1
phvohieunghia <phvohieunghia@gmail.com>
2022-07-01 17:39:41 +0700

    finsih copy ui

--- src/app/collects/detail/index.tsx
+++ src/app/collects/detail/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import clsx from 'clsx'
 import 'prismjs/themes/prism-okaidia.css'
 import Prism from 'prismjs'
@@ -6,28 +6,18 @@ import style from './css.module.scss'
 import { useAppSelector, useAppDispatch } from 'hooks/hooks'
 import Icons from 'assets/icons'
 import slice from './store'
-import Button from 'components/button'
+import CopyButton from 'components/button-copy'
 
 const CollectDetail = () => {
-	const mainRef = useRef<HTMLDivElement>(null)
-	const headingRef = useRef<HTMLDivElement>(null)
+	const copyButtonRef = useRef<HTMLButtonElement>(null)
 	const dispatch = useAppDispatch()
 	const state = useAppSelector((state) => state.collectDetail)
-	const [heightCode, setHeightCode] = useState<string>('0')
-	useEffect(() => {
-		const mainHeight = mainRef.current?.getBoundingClientRect().height as number
-		console.log(mainHeight)
-		const headingHeight = headingRef.current?.getBoundingClientRect()
-			.height as number
-		console.log(headingHeight)
-		setHeightCode(mainHeight - headingHeight - 30 + 'px')
-	})
-
 	useEffect(() => {
 		Prism.highlightAll()
-	})
+	}, [state.status])
 	function handleCopy() {
 		navigator.clipboard.writeText(state.value)
+		copyButtonRef.current?.click()
 	}
 	return (
 		<>
@@ -39,11 +29,8 @@ const CollectDetail = () => {
 							slice.actions.closeCollectDetail({ value: '', icon: null })
 						)
 					}>
-					<div
-						className={style.wrapper}
-						ref={mainRef}
-						onClick={(e) => e.stopPropagation()}>
-						<div className={style.head} ref={headingRef}>
+					<div className={style.wrapper} onClick={(e) => e.stopPropagation()}>
+						<div className={style.head}>
 							<span
 								onClick={() =>
 									dispatch(
@@ -54,17 +41,16 @@ const CollectDetail = () => {
 							</span>
 						</div>
 						<div className={clsx('container', style.content)}>
-							<div className={clsx(style.icon, 'col-4 col-md-12')}>
+							<div className={clsx(style.icon, 'col-3 col-md-12')}>
 								{state.icon}
 							</div>
 							<div
-								className={clsx(style.code, 'col-8 col-md-12')}
+								className={clsx(style.code, 'col-9 col-md-12')}
 								onClick={handleCopy}>
 								<span className={style.button}>
-									<Button>Copy</Button>
-									<Button>Copied</Button>
+									<CopyButton ref={copyButtonRef} />
 								</span>
-								<pre style={{ height: heightCode }}>
+								<pre style={{ maxHeight: window.innerHeight * 0.8 - 45 - 30 }}>
 									<code className='language-markup'>{state.value}</code>
 								</pre>
 							</div>
--- src/components/button-copy/index.tsx
+++ src/components/button-copy/index.tsx
@@ -1,13 +1,37 @@
-import clsx from 'clsx'
-import style from './css.module.scss'
+import { forwardRef } from 'react'
+import { useState, useLayoutEffect, memo } from 'react'
 
-const ButtonCopy = () => {
+import style from './css.module.scss'
+import Icons from 'assets/icons'
+
+const ButtonCopy = (props: any, ref: any) => {
+	const [state, setState] = useState<number>(0)
+	useLayoutEffect(() => {
+		if (state === 1) {
+			const timeoutId: ReturnType<typeof setTimeout> = setTimeout(() => {
+				setState((pre) => pre + 1)
+				clearTimeout(timeoutId)
+			}, 300)
+		} else if (state === 2) {
+			const timeoutId: ReturnType<typeof setTimeout> = setTimeout(() => {
+				setState((pre) => pre + 1)
+				clearTimeout(timeoutId)
+			}, 2000)
+		} else if (state > 2) {
+			setState(0)
+		}
+		return
+	}, [state])
 	return (
-		<button className={style.button}>
-			<span>Copy!</span>
-			<span>Copied!</span>
+		<button
+			className={style.button}
+			onClick={() => setState((pre) => pre + 1)}
+			ref={ref}>
+			{state === 0 && <Icons.Copy />}
+			{state === 1 && <Icons.Loading />}
+			{state === 2 && <Icons.Check />}
 		</button>
 	)
 }
 
-export default ButtonCopy
+export default memo(forwardRef(ButtonCopy))
