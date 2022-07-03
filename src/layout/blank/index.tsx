9411ba49d8f1a07e1dcb3d6448aa782097e3ccb9
phvohieunghia <phvohieunghia@gmail.com>
2022-07-03 17:33:57 +0700

    test google login

--- src/layout/blank/index.tsx
+++ src/layout/blank/index.tsx
@@ -1,11 +1,14 @@
 import clsx from 'clsx'
 import style from './css.module.scss'
 import FacebookLogin from 'react-facebook-login'
+import { GoogleLogin } from 'react-google-login'
 
 const responseFacebook = (response: any) => {
 	console.log(response)
 }
-
+const responseGoogle = (response: any) => {
+	console.log(response)
+}
 const BlankPage = () => {
 	function handleFacebook() {
 		console.log('click')
@@ -19,6 +22,13 @@ const BlankPage = () => {
 				fields='name,email,picture'
 				onClick={handleFacebook}
 				callback={responseFacebook}
+			/>
+			<GoogleLogin
+				clientId='269244489322-ubfba8u5g3p991eu3ci87icdov6pfpqi.apps.googleusercontent.com'
+				buttonText='Login'
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+				cookiePolicy={'single_host_origin'}
 			/>
 		</div>
 	)
