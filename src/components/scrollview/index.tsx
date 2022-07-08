22a560da598e37ee72a09b7405ef27019663767e
phvohieunghia <phvohieunghia@gmail.com>
2022-07-08 13:08:17 +0700

    fix scroll

--- src/components/scrollview/index.tsx
+++ src/components/scrollview/index.tsx
@@ -1,23 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 interface PropsType {
 	children: any
 }
 const ScrollView = (props: PropsType) => {
 	const { children } = props
+	const scrollRef = useRef<HTMLDivElement>(null)
 	const screenHeight = window.screen.height
 	function HandleScrolling(e: WheelEvent) {
-		if (e.deltaY > 0 && window.scrollY < screenHeight)
+		if (e.deltaY > 0 && window.scrollY < screenHeight) {
+			e.preventDefault()
 			window.scrollTo({ top: window.screen.height, behavior: 'smooth' })
-		else if (e.deltaY < 0 && window.scrollY < screenHeight) {
+		} else if (e.deltaY < 0 && window.scrollY <= screenHeight) {
+			e.preventDefault()
 			window.scrollTo({ top: 0, behavior: 'smooth' })
+			// scrollRef.current?.scrollIntoView(true)
 		}
 	}
-	window.addEventListener('wheel', (e) => HandleScrolling(e))
+	window.addEventListener('wheel', (e) => HandleScrolling(e), {
+		passive: false,
+	})
 	useEffect(() => {
 		return () => window.removeEventListener('wheel', (e) => HandleScrolling(e))
 	})
-	return <div>{children}</div>
+	return <div ref={scrollRef}>{children}</div>
 }
 
 export default ScrollView
